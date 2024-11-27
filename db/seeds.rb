c3e53efe5286637cee17dcb7114466398e4e5a44
YahiaJabeur <yahia.jabeur@gmail.com>
2024-11-27 15:04:59 +0100

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+Restaurant.create!(
+  name: "Italian Bistro",
+  address: "123 Pasta St, Roma, Italy",
+  phone_number: "+39 06 1234567",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Sushi World",
+  address: "456 Fish Lane, Tokyo, Japan",
+  phone_number: "+81 3 2345678",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Le Petit Paris",
+  address: "789 Rue de Paris, Paris, France",
+  phone_number: "+33 1 3456789",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Biergarten Berlin",
+  address: "101 Brewster Ave, Berlin, Germany",
+  phone_number: "+49 30 9876543",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Peking Duck House",
+  address: "202 Duck Rd, Beijing, China",
+  phone_number: "+86 10 8765432",
+  category: "chinese"
+)
